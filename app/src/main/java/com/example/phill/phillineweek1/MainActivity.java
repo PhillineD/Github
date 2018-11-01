05ba8abc00424296cf889903152a884b5b397474
Philline <fidik@live.nl>
2018-11-01 12:19:52 +0100

    Initial project

--- app/src/main/java/com/example/phill/phillineweek1/MainActivity.java
+++ app/src/main/java/com/example/phill/phillineweek1/MainActivity.java
@@ -29,79 +29,119 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void checkClicked(View view) {
-        // Is the view now checked?
-//        Boolean checked = ((CheckBox) view).isChecked();
+
+        // check all checkboxes if they are clicked, if so then change the view of it
         switch(view.getId()) {
             case R.id.checkBoxArms:
                 imageViewArms = (ImageView) findViewById(R.id.imageViewArms);
+
+                // if the image is not visible make if visible
                 if( imageViewArms.getVisibility() == View.INVISIBLE)
                     imageViewArms.setVisibility(View.VISIBLE);
+
+                // if the image is visible make it not visible
                 else
                     imageViewArms.setVisibility(View.INVISIBLE);
                 break;
             case R.id.checkBoxEars:
                 imageViewEars = (ImageView) findViewById(R.id.imageViewEars);
+
+                // if the image is not visible make if visible
                 if( imageViewEars.getVisibility() == View.INVISIBLE)
                     imageViewEars.setVisibility(View.VISIBLE);
+
+                // if the image is visible make it not visible
                 else
                     imageViewEars.setVisibility(View.INVISIBLE);
                 break;
             case R.id.checkBoxEyes:
                 imageViewEyes = (ImageView) findViewById(R.id.imageViewEyes);
+
+                // if the image is not visible make if visible
                 if( imageViewEyes.getVisibility() == View.INVISIBLE)
                     imageViewEyes.setVisibility(View.VISIBLE);
+
+                // if the image is visible make it not visible
                 else
                     imageViewEyes.setVisibility(View.INVISIBLE);
                 break;
             case R.id.checkBoxEyebrows:
                 imageViewEyebrows = (ImageView) findViewById(R.id.imageViewEyebrows);
+
+                // if the image is not visible make if visible
                 if( imageViewEyebrows.getVisibility() == View.INVISIBLE)
                     imageViewEyebrows.setVisibility(View.VISIBLE);
+
+                // if the image is visible make it not visible
                 else
                     imageViewEyebrows.setVisibility(View.INVISIBLE);
                 break;
             case R.id.checkBoxGlasses:
                 imageViewGlasses = (ImageView) findViewById(R.id.imageViewGlasses);
+
+                // if the image is not visible make if visible
                 if( imageViewGlasses.getVisibility() == View.INVISIBLE)
                     imageViewGlasses.setVisibility(View.VISIBLE);
+
+                // if the image is visible make it not visible
                 else
                     imageViewGlasses.setVisibility(View.INVISIBLE);
                 break;
             case R.id.checkBoxNose:
                 imageViewNose = (ImageView) findViewById(R.id.imageViewNose);
+
+                // if the image is not visible make if visible
                 if( imageViewNose.getVisibility() == View.INVISIBLE)
                     imageViewNose.setVisibility(View.VISIBLE);
+
+                // if the image is visible make it not visible
                 else
                     imageViewNose.setVisibility(View.INVISIBLE);
                 break;
 
             case R.id.checkBoxShoes:
                 imageViewshoes = (ImageView) findViewById(R.id.imageViewshoes);
+
+                // if the image is not visible make if visible
                 if( imageViewshoes.getVisibility() == View.INVISIBLE)
                     imageViewshoes.setVisibility(View.VISIBLE);
+
+                // if the image is visible make it not visible
                 else
                     imageViewshoes.setVisibility(View.INVISIBLE);
                 break;
 
             case R.id.checkBoxMustage:
                 imageViewMustage = (ImageView) findViewById(R.id.imageViewMustage);
+
+                // if the image is not visible make if visible
                 if( imageViewMustage.getVisibility() == View.INVISIBLE)
                     imageViewMustage.setVisibility(View.VISIBLE);
+
+                // if the image is visible make it not visible
                 else
                     imageViewMustage.setVisibility(View.INVISIBLE);
                 break;
 
             case R.id.checkBoxMouth:
                 imageViewMouth = (ImageView) findViewById(R.id.imageViewMouth);
+
+                // if the image is not visible make if visible
                 if( imageViewMouth.getVisibility() == View.INVISIBLE)
                     imageViewMouth.setVisibility(View.VISIBLE);
+
+                // if the image is visible make it not visible
                 else
                     imageViewMouth.setVisibility(View.INVISIBLE);
                 break;
             case R.id.checkBoxHed:
                 imageViewHed = (ImageView) findViewById(R.id.imageViewHed);
+
+                // if the image is not visible make if visible
                 if( imageViewHed.getVisibility() == View.INVISIBLE)
                     imageViewHed.setVisibility(View.VISIBLE);
+
+                // if the image is visible make it not visible
                 else
                     imageViewHed.setVisibility(View.INVISIBLE);
                 break;
