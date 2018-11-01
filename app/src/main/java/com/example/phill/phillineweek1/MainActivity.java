039bf9bd44666dc0a94fea942909a93d7f167ca4
Philline <fidik@live.nl>
2018-11-01 17:05:21 +0100

    Initial project

--- app/src/main/java/com/example/phill/phillineweek1/MainActivity.java
+++ app/src/main/java/com/example/phill/phillineweek1/MainActivity.java
@@ -26,6 +26,64 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageViewHed = (ImageView) findViewById(R.id.imageViewHed);
+        imageViewEars = (ImageView) findViewById(R.id.imageViewEars);
+        imageViewArms = (ImageView) findViewById(R.id.imageViewArms);
+        imageViewNose = (ImageView) findViewById(R.id.imageViewNose);
+        imageViewGlasses = (ImageView) findViewById(R.id.imageViewGlasses);
+        imageViewshoes = (ImageView) findViewById(R.id.imageViewshoes);
+        imageViewMouth = (ImageView) findViewById(R.id.imageViewMouth);
+        imageViewMustage = (ImageView) findViewById(R.id.imageViewMustage);
+        imageViewEyes = (ImageView) findViewById(R.id.imageViewEyes);
+        imageViewEyebrows = (ImageView) findViewById(R.id.imageViewEyebrows);
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.getInt("Arms") == 0) {
+                imageViewArms.setVisibility(View.VISIBLE);
+            }
+            if (savedInstanceState.getInt("Ears") == 0) {
+                imageViewEars.setVisibility(View.VISIBLE);
+            }
+            if (savedInstanceState.getInt("Eyebrows") == 0) {
+                imageViewEyebrows.setVisibility(View.VISIBLE);
+            }
+            if (savedInstanceState.getInt("Eyes") == 0) {
+                imageViewEyes.setVisibility(View.VISIBLE);
+            }
+            if (savedInstanceState.getInt("Glasses") == 0) {
+                imageViewGlasses.setVisibility(View.VISIBLE);
+            }
+            if (savedInstanceState.getInt("Hat") == 0) {
+                imageViewHed.setVisibility(View.VISIBLE);
+            }
+            if (savedInstanceState.getInt("Mouth") == 0) {
+                imageViewMouth.setVisibility(View.VISIBLE);
+            }
+            if (savedInstanceState.getInt("Mustache") == 0) {
+                imageViewMustage.setVisibility(View.VISIBLE);
+            }
+            if (savedInstanceState.getInt("Nose") == 0) {
+                imageViewNose.setVisibility(View.VISIBLE);
+            }
+            if (savedInstanceState.getInt("Shoes") == 0) {
+                imageViewshoes.setVisibility(View.VISIBLE);
+            }
+        }
+    }
+
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState); // always call super
+        outState.putInt("Hat", imageViewHed.getVisibility());
+        outState.putInt("Ears", imageViewEars.getVisibility());
+        outState.putInt("Arms", imageViewArms.getVisibility());
+        outState.putInt("Nose", imageViewNose.getVisibility());
+        outState.putInt("Glasses", imageViewGlasses.getVisibility());
+        outState.putInt("Shoes", imageViewshoes.getVisibility());
+        outState.putInt("Mouth", imageViewMouth.getVisibility());
+        outState.putInt("Mustache", imageViewMustage.getVisibility());
+        outState.putInt("Eyes", imageViewEyes.getVisibility());
+        outState.putInt("Eyebrows", imageViewEyebrows.getVisibility());
     }
 
     public void checkClicked(View view) {
@@ -36,10 +94,10 @@ public class MainActivity extends AppCompatActivity {
                 imageViewArms = (ImageView) findViewById(R.id.imageViewArms);
 
                 // if the image is not visible make if visible
-                if( imageViewArms.getVisibility() == View.INVISIBLE)
+                if (imageViewArms.getVisibility() == View.INVISIBLE)
                     imageViewArms.setVisibility(View.VISIBLE);
 
-                // if the image is visible make it not visible
+                    // if the image is visible make it not visible
                 else
                     imageViewArms.setVisibility(View.INVISIBLE);
                 break;
@@ -47,10 +105,10 @@ public class MainActivity extends AppCompatActivity {
                 imageViewEars = (ImageView) findViewById(R.id.imageViewEars);
 
                 // if the image is not visible make if visible
-                if( imageViewEars.getVisibility() == View.INVISIBLE)
+                if (imageViewEars.getVisibility() == View.INVISIBLE)
                     imageViewEars.setVisibility(View.VISIBLE);
 
-                // if the image is visible make it not visible
+                    // if the image is visible make it not visible
                 else
                     imageViewEars.setVisibility(View.INVISIBLE);
                 break;
@@ -58,10 +116,10 @@ public class MainActivity extends AppCompatActivity {
                 imageViewEyes = (ImageView) findViewById(R.id.imageViewEyes);
 
                 // if the image is not visible make if visible
-                if( imageViewEyes.getVisibility() == View.INVISIBLE)
+                if (imageViewEyes.getVisibility() == View.INVISIBLE)
                     imageViewEyes.setVisibility(View.VISIBLE);
 
-                // if the image is visible make it not visible
+                    // if the image is visible make it not visible
                 else
                     imageViewEyes.setVisibility(View.INVISIBLE);
                 break;
@@ -69,10 +127,10 @@ public class MainActivity extends AppCompatActivity {
                 imageViewEyebrows = (ImageView) findViewById(R.id.imageViewEyebrows);
 
                 // if the image is not visible make if visible
-                if( imageViewEyebrows.getVisibility() == View.INVISIBLE)
+                if (imageViewEyebrows.getVisibility() == View.INVISIBLE)
                     imageViewEyebrows.setVisibility(View.VISIBLE);
 
-                // if the image is visible make it not visible
+                    // if the image is visible make it not visible
                 else
                     imageViewEyebrows.setVisibility(View.INVISIBLE);
                 break;
@@ -80,10 +138,10 @@ public class MainActivity extends AppCompatActivity {
                 imageViewGlasses = (ImageView) findViewById(R.id.imageViewGlasses);
 
                 // if the image is not visible make if visible
-                if( imageViewGlasses.getVisibility() == View.INVISIBLE)
+                if (imageViewGlasses.getVisibility() == View.INVISIBLE)
                     imageViewGlasses.setVisibility(View.VISIBLE);
 
-                // if the image is visible make it not visible
+                    // if the image is visible make it not visible
                 else
                     imageViewGlasses.setVisibility(View.INVISIBLE);
                 break;
@@ -91,10 +149,10 @@ public class MainActivity extends AppCompatActivity {
                 imageViewNose = (ImageView) findViewById(R.id.imageViewNose);
 
                 // if the image is not visible make if visible
-                if( imageViewNose.getVisibility() == View.INVISIBLE)
+                if (imageViewNose.getVisibility() == View.INVISIBLE)
                     imageViewNose.setVisibility(View.VISIBLE);
 
-                // if the image is visible make it not visible
+                    // if the image is visible make it not visible
                 else
                     imageViewNose.setVisibility(View.INVISIBLE);
                 break;
@@ -103,10 +161,10 @@ public class MainActivity extends AppCompatActivity {
                 imageViewshoes = (ImageView) findViewById(R.id.imageViewshoes);
 
                 // if the image is not visible make if visible
-                if( imageViewshoes.getVisibility() == View.INVISIBLE)
+                if (imageViewshoes.getVisibility() == View.INVISIBLE)
                     imageViewshoes.setVisibility(View.VISIBLE);
 
-                // if the image is visible make it not visible
+                    // if the image is visible make it not visible
                 else
                     imageViewshoes.setVisibility(View.INVISIBLE);
                 break;
@@ -115,10 +173,10 @@ public class MainActivity extends AppCompatActivity {
                 imageViewMustage = (ImageView) findViewById(R.id.imageViewMustage);
 
                 // if the image is not visible make if visible
-                if( imageViewMustage.getVisibility() == View.INVISIBLE)
+                if (imageViewMustage.getVisibility() == View.INVISIBLE)
                     imageViewMustage.setVisibility(View.VISIBLE);
 
-                // if the image is visible make it not visible
+                    // if the image is visible make it not visible
                 else
                     imageViewMustage.setVisibility(View.INVISIBLE);
                 break;
@@ -127,10 +185,10 @@ public class MainActivity extends AppCompatActivity {
                 imageViewMouth = (ImageView) findViewById(R.id.imageViewMouth);
 
                 // if the image is not visible make if visible
-                if( imageViewMouth.getVisibility() == View.INVISIBLE)
+                if (imageViewMouth.getVisibility() == View.INVISIBLE)
                     imageViewMouth.setVisibility(View.VISIBLE);
 
-                // if the image is visible make it not visible
+                    // if the image is visible make it not visible
                 else
                     imageViewMouth.setVisibility(View.INVISIBLE);
                 break;
@@ -138,14 +196,15 @@ public class MainActivity extends AppCompatActivity {
                 imageViewHed = (ImageView) findViewById(R.id.imageViewHed);
 
                 // if the image is not visible make if visible
-                if( imageViewHed.getVisibility() == View.INVISIBLE)
+                if (imageViewHed.getVisibility() == View.INVISIBLE)
                     imageViewHed.setVisibility(View.VISIBLE);
 
-                // if the image is visible make it not visible
+                    // if the image is visible make it not visible
                 else
                     imageViewHed.setVisibility(View.INVISIBLE);
                 break;
 
         }
     }
+
 }
